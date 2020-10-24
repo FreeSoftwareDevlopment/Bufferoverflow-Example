e2ffd9ce1ef343b3408f25376d6ea8f87875f8b3
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-10-24 10:29:57 +0200

    Add bufferoverflow project

---
+++ bufferoverflow.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char hey[500];
+	cin >> hey;
+	cout << "\n\n################\n\n" << hey << flush;
+	cout << "\n\n COMPLETED" << endl;
+	return 0;
+}
