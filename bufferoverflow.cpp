1e9dbb725bc9aff3263b89bbfd26198f36de83b7
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-10-24 11:00:27 +0200

    Add Comments to Source

--- bufferoverflow.cpp
+++ bufferoverflow.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
-using namespace std;
-int main(){
-	char hey[500];
-	cin >> hey;
-	cout << "\n\n################\n\n" << hey << flush;
-	cout << "\n\n COMPLETED" << endl;
-	return 0;
+/*
+ * <c> Sharkbyteprojects
+ * MIT LICENSE
+ * https://github.com/FreeSoftwareDevlopment/Bufferoverflow-Example/
+ */
+#include <iostream>  //INCLUDE INPUT/OUTPUT STREAM LIB
+using namespace std; //AFTER THIS, cout == std::cout and flush == std::flush
+int main(){ // MAIN function
+	char hey[500];// buffer "char[500]"
+	cin >> hey; //stdin to buffer
+	cout << "\n\n################\n\n" << hey << flush; // PRINT THE CONTENT OF BUFFER AND FLUSH IT OUT
+	cout << "\n\n COMPLETED" << endl;//Show "Completed"
+	return 0; // Return 0 | "All OK" Code
 }
