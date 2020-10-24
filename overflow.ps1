77a97bb4b0164c72fe9fec4c5c5a8335a70472a9
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-10-24 11:01:50 +0200

    Update overflow.ps1

--- overflow.ps1
+++ overflow.ps1
@@ -1,4 +1,4 @@
-$overflowcontent=(("0f0f"*800)+"xx");
+$overflowcontent=(("0f0f"*800)+"xx")
 Write-Output "SharkOverflow" "Content of file bufferoverflow.cpp:"
 Get-Content "bufferoverflow.cpp" | Write-Output
 Write-Output "" 'Run the file "./bufferoverflow.exe" with the stdin (("0f0f"*800)+"xx") in a pipe' "Wait for COMPLETED! IF COMPLETED DOSN'T SHOW UP, PROGRAM IS DESTROYED BY OVERFLOW" "Note: In Sandboxie you can see the error SBIE2101 " "---" ""
